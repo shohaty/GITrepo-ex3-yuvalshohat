44a2d97a093f69386199491223482cb21fddb96d
shohaty <shohaty@gmail.com>
2022-11-23 14:02:09 +0200

    changing prints

--- yuvalshohat.java
+++ yuvalshohat.java
@@ -1,7 +1,7 @@
 public class yuvalshohat{
 public static void main(String[] args) {
 for (int i=1;i<=5; i++) {
-System.out.println("Yuval Shohat"+i);
+System.out.println("Yuval Shohat studying Devops"+i);
 }
 }
 }
